16d8e4e7e3787ce6c118a37b5394f449461eafef
ACER <hendry45xtc@gmail.com>
2021-12-22 01:14:37 +0700

    Menghapus TodoList

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+    testAddTodoList();
 
     }
 
@@ -64,54 +64,30 @@ public class Main {
                 break;
             }
         }
-
-
     }
     //test addTodoList
-//    public static void testAddTodoList(){
-//        for (int i = 0; i < 25; i++){
-//            addTodoList("todo ke " + i);
-//        }
-//
-//        showTodoList();
-//    }
+    public static void testAddTodoList(){
+        for (int i = 0; i < 25; i++){
+            addTodoList("todo ke " + i);
+        }
+
+        showTodoList();
+    }
 
     /**
      * Menghapus todo dari list
      */
 
-    public static boolean removeTodoList(Integer number){
-        if ((number - 1) >= model.length){
+    public static boolean removeTodoList(Integer number) {
+        if ((number - 1) >= model.length) {
             return false;
-        }else if (model[number - 1] == null){
+        } else if (model[number - 1] == null) {
             return false;
-        }else {
-            for (int i = (number -1); i < model.length; i++){
-                if (i == (model.length - 1)){
-                    model[i] = null;
-                }else {
-                    model[i] = model [i + 1];
-                }
-            }
+        } else {
+            model[number - 1] = null;
             return true;
         }
     }
-    //test Remove TodoList
-//    public static void testRemoveTodoList(){
-//        addTodoList("satu");
-//        addTodoList("dua");
-//        addTodoList("Tiga");
-//        addTodoList("Empat");
-//        addTodoList("Lima");
-//
-//        var result = removeTodoList(20);
-//        System.out.println(result);
-//
-//        result = removeTodoList(2);
-//        System.out.println(result);
-//
-//        showTodoList();
-//    }
 
     /**
      * Menampilkan view todo list
